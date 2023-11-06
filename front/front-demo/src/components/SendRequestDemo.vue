f3c04f8991d650525896fa2ce4e31bfe345e99e3
winspace <634739210@qq.com>
2023-11-06 17:32:36 +0800

    添加elementui

---
+++ front/front-demo/src/components/SendRequestDemo.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+     <el-button size="small" type="primary">发送请求</el-button>
+    </div>
+  </template>
+  
+  <script>
+  
+  
+  export default {
+    name: 'SendRequestDemo',
+    mounted(){
+        this.sendRequest()
+    },  
+    methods:{
+        sendRequest(){
+            // alert(1)
+        }
+    }
+  }
+  </script>
+  
--- front/front-demo/src/main.js
+++ front/front-demo/src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import elementui from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.config.productionTip = false
+
+Vue.use(elementui)
 
 new Vue({
   render: h => h(App),
