4227ad7f3b088e48b77b7017d8e073b033a08bc6
winspace <634739210@qq.com>
2023-11-16 19:34:11 +0800

    文件的上传和下载都可以了

--- end/springTest/src/main/java/com/example/springtest/controller/DatabaseController.java
+++ end/springTest/src/main/java/com/example/springtest/controller/DatabaseController.java
@@ -1,32 +1,45 @@
 package com.example.springtest.controller;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+import org.json.JSONObject;
+import org.json.JSONArray;
+
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
 
 @RestController
 @RequestMapping("/api")
 public class DatabaseController {
 
+    @Autowired
+    private DataSource dataSource; // 注入数据源
+
+    private static final String FILE_UPLOAD_PATH = "C:/uploads/"; // 设置文件上传路径
 
     @GetMapping("/data")
     public List<String> getData() {
         List<String> result = new ArrayList<>();
 
-        // Replace the following connection details with your actual database connection information
-        String url = "jdbc:mysql://localhost:3306/test";
-        String username = "root";
-        String password = "123456";
-
-        String query = "SELECT name FROM testtable";
-
-        try (Connection connection = DriverManager.getConnection(url, username, password);
+        try (Connection connection = dataSource.getConnection();
              Statement statement = connection.createStatement();
-             ResultSet resultSet = statement.executeQuery(query)) {
+             ResultSet resultSet = statement.executeQuery("SELECT name FROM testtable")) {
 
             while (resultSet.next()) {
                 String data = resultSet.getString("name");
@@ -34,10 +47,89 @@ public class DatabaseController {
             }
 
         } catch (SQLException e) {
-            // Handle any potential exceptions here
-//            e.printStackTrace();
+            e.printStackTrace();
         }
 
         return result;
+    }
+
+    @PostMapping("/upload")
+    public String uploadFile(@RequestParam("file") MultipartFile file) {
+        if (file.isEmpty()) {
+            return "上传失败，请选择文件";
+        }
+
+        try {
+            // 生成唯一的文件名
+            String fileName = file.getOriginalFilename();
+            // 构建目标文件对象
+            File destFile = new File(fileName);
+            // 将上传文件保存到目标文件
+            FileCopyUtils.copy(file.getInputStream(), new FileOutputStream(destFile));
+
+            // 将文件信息插入数据库
+            try (Connection connection = dataSource.getConnection();
+                 PreparedStatement statement = connection.prepareStatement("INSERT INTO files (filename, file_content) VALUES (?, ?)")) {
+                statement.setString(1, fileName);
+                statement.setBinaryStream(2, file.getInputStream());
+                statement.executeUpdate();
+            }
+
+            return "文件上传成功，文件名：" + fileName;
+        } catch (IOException | SQLException e) {
+            e.printStackTrace();
+            return "上传失败：" + e.getMessage();
+        }
+    }
+
+    @GetMapping("/filelist")
+    public String getFileList() {
+        JSONArray jsonArray = new JSONArray();
+
+        try (Connection connection = dataSource.getConnection();
+             Statement statement = connection.createStatement();
+             ResultSet resultSet = statement.executeQuery("SELECT filename FROM files")) {
+
+            while (resultSet.next()) {
+                String fileName = resultSet.getString("filename");
+                JSONObject object1 = new JSONObject();
+                object1.put("fileName",fileName);
+                jsonArray.put(object1);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return jsonArray.toString();
+    }
+
+    @GetMapping("/download/{fileName}")
+    public ResponseEntity<Resource> downloadFile(@PathVariable String fileName) {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement("SELECT * FROM files WHERE filename = ?")) {
+            statement.setString(1, fileName);
+            ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                // Retrieve the file content from the database
+                Blob fileContent = resultSet.getBlob("file_content");
+                // Convert the file content to a byte array
+                byte[] bytes = fileContent.getBytes(1, (int) fileContent.length());
+                // Create a ByteArrayResource
+                ByteArrayResource resource = new ByteArrayResource(bytes);
+                // Set the HTTP headers
+                HttpHeaders headers = new HttpHeaders();
+                headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileName + "\"");
+                headers.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_OCTET_STREAM_VALUE);
+                headers.add(HttpHeaders.CONTENT_LENGTH, String.valueOf(bytes.length));
+                // Return the ResponseEntity
+                return ResponseEntity.ok()
+                        .headers(headers)
+                        .body(resource);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return ResponseEntity.notFound().build();
     }
 }
--- front/front-demo/src/components/SendRequestDemo.vue
+++ front/front-demo/src/components/SendRequestDemo.vue
@@ -3,6 +3,23 @@
      <el-button size="small" type="primary" @click="sendRequest">发送请求hello</el-button>
      <el-button size="small" type="primary" @click="sendRequestGreet">打招呼json</el-button>
      <el-button size="small" type="primary" @click="sendRequestDatabase">数据库请求</el-button>
+     <el-button size="small" type="primary" @click="refreshFileList">刷新文件列表</el-button>
+     <el-upload
+      action="/api/upload"  
+      :before-upload="beforeUpload"
+      :on-success="onSuccess" 
+      :on-error="onError"
+    >
+      <el-button size="small" type="primary">点击上传</el-button>
+    </el-upload>
+    <el-table :data="fileList" height="300">
+      <el-table-column prop="fileName" label="文件名"></el-table-column>
+      <el-table-column label="操作">
+        <template slot-scope="scope">
+          <el-button type="primary" size="small" @click="downloadFile(scope.row.fileName)">下载</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
     </div>
   </template>
   
@@ -12,7 +29,27 @@
 
   export default {
     name: 'SendRequestDemo',
+    data(){
+        return {
+            fileList:[{"fileName":1}]
+        }
+    },  
     methods:{
+        beforeUpload(file) {
+            // 在上传前对文件进行验证，例如文件类型、大小等
+            // 如果验证失败，返回 false 可以阻止文件上传
+            // 如果验证成功，返回 true 允许文件上传
+            console.log('beforeUpload', file);
+            return true;
+        },
+        onSuccess(response, file) {
+            // 上传成功后的回调方法
+            console.log('onSuccess', response, file);
+        },
+        onError(error, file) {
+            // 上传失败后的回调方法
+            console.log('onError', error, file);
+        },
         sendRequestGreet(){
             axios.get('/greeting?name="john"', { 
             // 请求体数据
@@ -54,7 +91,32 @@
                 // 请求失败，处理错误
                 console.error(error);
             });
+        },
+        refreshFileList(){
+            axios.get('/api/filelist', { 
+            // 请求体数据
+            // 可以是对象、数组或其他类型的数据
+            })
+            .then(response => {
+                // 请求成功，处理响应数据
+                this.fileList = response.data
+                
+            })
+            .catch(error => {
+                // 请求失败，处理错误
+                console.error(error);
+            });
+        },
+        downloadFile(filename) {
+            // 下载文件的逻辑
+            // 创建一个链接，并设置其 href 属性为下载文件的后端接口路径
+            const link = document.createElement('a');
+            link.href = `/api/download/${filename}`;
+            link.download = filename;
+            // 触发点击事件来下载文件
+            link.click();
         }
+
     }
   }
   </script>
